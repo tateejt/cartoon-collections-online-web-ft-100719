a9d3d88d0f2ab7c1f32f643f1c0cad29d618dc5b
Emery Tate <tateejt@gmail.com>
2019-10-13 23:02:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,13 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
+    i = 0 
+  if  array.any? {|i| i.length > 4}
+    return true
+  else 
+    return false
+  i = i + 1
+  end 
 end
 
 def find_the_cheese# code an argument here
