3b2010307a6e83f581c51597f5995b9daf536318
Emery Tate <tateejt@gmail.com>
2019-10-13 22:59:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,9 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
-  array.each |string| 
+  i = 0 
+  while i < array.length 
+    puts "#{i + 1}. #{array[i]}"
+    i += 1
 end
 
 def summon_captain_planet# code an argument here
